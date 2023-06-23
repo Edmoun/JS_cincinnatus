6dec62b76eb3e70b062691eb9021c6718bdab1eb
Edmoun Ramirez <edmounramirez@gmail.com>
2023-06-22 21:14:12 -0400

    Calculadora(mi_primer_proyecto)

---
+++ metodos.js
@@ -0,0 +1,13 @@
+// forEach: funcion que me permite iterar sobre cada uno de los elementos de un array.
+// some: Esta funcion me muestra los numeros que estan en mi array.
+// every: Esta funcion me dice los numeros que no estan en mi array.
+// map: me permite extraer los elementos de mi array y hacer con ellos lo que quiera.
+// filter: me permite crear un sub-arreglo de mi arreglo, claro cumpliendo la condicion que se le ponga.
+
+
+let numero = [1,2,3,4,5,6,7];
+
+for (let i = 0; i < numero.length -1; i++) {
+    console.log(numero[i])
+    
+}
